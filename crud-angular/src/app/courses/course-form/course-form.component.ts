f8283eb0794fdca3dc43b46b2ac63a8f9cf2950f
Stefano Paulo <stefanopaulo47@gmail.com>
2022-05-03 15:57:44 -0300

    Form: Spring API: Create Course (HTTP POST)

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -16,7 +17,8 @@ export class CourseFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -29,11 +31,17 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value)
-        .subscribe(result => console.log(result), _error => this.onError());
+        .subscribe(result => this.onSuccess(), _error => this.onError());
   }
 
   onCancel() {
+    this.location.back();
+  }
 
+
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso.', '', { duration: 3000 });
+    this.onCancel()
   }
 
   private onError() {
--- crud-spring/src/main/java/com/stefano/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/stefano/crudspring/controller/CourseController.java
@@ -5,8 +5,13 @@ import java.util.List;
 import com.stefano.crudspring.model.Course;
 import com.stefano.crudspring.repository.CourseRepository;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -21,5 +26,11 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 }
