3888f99c888f6e45989e0a0a7944c01ac1e2e181
spsouza <stefanopaulo47@gmail.com>
2022-10-01 09:29:55 -0300

    Update Course: Saving the Form

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -26,7 +26,20 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>) {
+    if(record._id) {
+      return this.update(record);
+    }
+
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>) {
     return this.http.post<Course>(this.API, record)
+    .pipe(first());
+  }
+
+  private update(record: Partial<Course>) {
+    return this.http.put<Course>(`${this.API}/${record._id}`, record)
     .pipe(first());
   }
 }
