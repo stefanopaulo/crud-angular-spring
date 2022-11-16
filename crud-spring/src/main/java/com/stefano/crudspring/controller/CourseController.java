bdaa47ea097e8165ed7e72f4358745fe3d5e05dc
spsouza <stefanopaulo47@gmail.com>
2022-11-16 10:58:46 -0300

    Update Course: Spring HTTP PUT API

--- crud-spring/src/main/java/com/stefano/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/stefano/crudspring/controller/CourseController.java
@@ -2,13 +2,12 @@ package com.stefano.crudspring.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -42,5 +41,17 @@ public class CourseController {
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course) {
         return courseRepository.save(course);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+    	return courseRepository.findById(id)
+    			.map(recordFound -> {
+    				recordFound.setName(course.getName());
+    				recordFound.setCategory(course.getCategory());
+    				Course updated = courseRepository.save(recordFound);
+    				return ResponseEntity.ok().body(updated);
+    			})
+    			.orElse(ResponseEntity.notFound().build());
     }
 }
