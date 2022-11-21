06571cf61c7f0d95cdeef5e1216b1fe13e48a4ff
spsouza <stefanopaulo47@gmail.com>
2022-11-21 18:47:12 -0300

    Remove Course: API Spring HTTP DELETE

--- crud-spring/src/main/java/com/stefano/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/stefano/crudspring/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,36 +23,41 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/api/courses")
 @AllArgsConstructor
 public class CourseController {
-    
-    private final CourseRepository courseRepository;
 
-    @GetMapping
-    public List<Course> list() {
-        return courseRepository.findAll();
-    }
-    
-    @GetMapping("/{id}")
-    public ResponseEntity<Course> findCourse(@PathVariable Long id) {
-    	return courseRepository.findById(id)
-    			.map(record -> ResponseEntity.ok(record))
-    			.orElse(ResponseEntity.notFound().build());
-    }
+	private final CourseRepository courseRepository;
 
-    @PostMapping
-    @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody Course course) {
-        return courseRepository.save(course);
-    }
-    
-    @PutMapping("/{id}")
-    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
-    	return courseRepository.findById(id)
-    			.map(recordFound -> {
-    				recordFound.setName(course.getName());
-    				recordFound.setCategory(course.getCategory());
-    				Course updated = courseRepository.save(recordFound);
-    				return ResponseEntity.ok().body(updated);
-    			})
-    			.orElse(ResponseEntity.notFound().build());
-    }
+	@GetMapping
+	public List<Course> list() {
+		return courseRepository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Course> findCourse(@PathVariable Long id) {
+		return courseRepository.findById(id).map(record -> ResponseEntity.ok(record))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Course create(@RequestBody Course course) {
+		return courseRepository.save(course);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+		return courseRepository.findById(id).map(recordFound -> {
+			recordFound.setName(course.getName());
+			recordFound.setCategory(course.getCategory());
+			Course updated = courseRepository.save(recordFound);
+			return ResponseEntity.ok().body(updated);
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		return courseRepository.findById(id).map(recordFound -> {
+			courseRepository.deleteById(id);
+			return ResponseEntity.noContent().<Void>build();
+		}).orElse(ResponseEntity.notFound().build());
+	}
 }
