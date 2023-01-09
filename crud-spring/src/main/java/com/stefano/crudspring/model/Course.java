26d7cb9ebdd7b638bff9a0372d5092bd8b65eeba
spsouza <stefanopaulo47@gmail.com>
2023-01-09 15:50:51 -0300

    Logical Removal (Soft Delete)

--- crud-spring/src/main/java/com/stefano/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/stefano/crudspring/model/Course.java
@@ -9,6 +9,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -17,6 +19,8 @@ import lombok.Data;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Course {
     
     @Id
@@ -35,4 +39,10 @@ public class Course {
     @Pattern(regexp = "Front-end|Back-end")
     @Column(length = 10, nullable = false)
     private String category;
+    
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
 }
