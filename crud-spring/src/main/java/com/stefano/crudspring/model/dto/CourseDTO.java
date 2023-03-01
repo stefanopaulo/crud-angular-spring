2eb5c07adf501404238203a02d3a62960bea3694
spsouza <stefanopaulo47@gmail.com>
2023-03-01 20:39:37 -0300

    Standard DTO (Data Transfer Object)

---
+++ crud-spring/src/main/java/com/stefano/crudspring/model/dto/CourseDTO.java
@@ -0,0 +1,29 @@
+package com.stefano.crudspring.model.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class CourseDTO {
+	
+	@JsonProperty("_id")
+	private Long id;
+
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 200)
+    private String name;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Front-end|Back-end")
+    private String category;
+
+}
